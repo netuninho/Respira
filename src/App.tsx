eb785ea61606d04aa2e0a03bf0076295c63b437d
Manu <manuelass.dev@gmail.com>
2025-10-19 00:11:51 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from 'react'
+
+export default function App() {
+  return (
+    <main className="flex flex-col items-center justify-center text-center gap-8 px-4 min-h-screen">
+      <h1>Testando</h1>
+    </main>
+  )
+}
